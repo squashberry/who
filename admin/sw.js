c66d593e8dd6d1f7644850f140386dfcacb15ca2
Squashberrypro <squashberrypro@gmail.com>
2026-09-24 07:44:28 +0000

    Cache WHO admin PWA icons

--- admin/sw.js
+++ admin/sw.js
@@ -1,5 +1,5 @@
 const CACHE='who-admin-v2';
-const CORE=['./','./index.html','./admin.css','./admin.js','./site.webmanifest','../favicon.svg'];
+const CORE=['./','./index.html','./admin.css','./admin.js','./site.webmanifest','../favicon.svg','../icons/icon-192.png','../icons/icon-512.png'];
 self.addEventListener('install',(e)=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)));self.skipWaiting()});
 self.addEventListener('activate',(e)=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
 self.addEventListener('fetch',(e)=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request).then(r=>{const cp=r.clone();caches.open(CACHE).then(x=>x.put(e.request,cp));return r}).catch(()=>caches.match('./index.html'))))});
