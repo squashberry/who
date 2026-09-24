ac96c0ec2011fcc917fa2ba83a1276b959704045
Squashberrypro <squashberrypro@gmail.com>
2026-09-24 07:44:25 +0000

    Cache WHO PWA home screen icons

--- sw.js
+++ sw.js
@@ -5,7 +5,9 @@ const CORE = [
   './app.css',
   './app.js',
   './favicon.svg',
-  './site.webmanifest'
+  './site.webmanifest',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
 ];
 
 self.addEventListener('install', (event) => {
