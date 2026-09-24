f4e9c533c1f08deaead2dc0d4b89ef48b4b7c138
Squashberrypro <squashberrypro@gmail.com>
2026-09-24 07:39:25 +0000

    Add lightweight WHO service worker

---
+++ sw.js
@@ -0,0 +1,39 @@
+const CACHE = 'who-web-v2';
+const CORE = [
+  './',
+  './index.html',
+  './app.css',
+  './app.js',
+  './favicon.svg',
+  './site.webmanifest'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(CORE)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  const request = event.request;
+  if (request.method !== 'GET') return;
+
+  event.respondWith(
+    caches.match(request).then((cached) => {
+      if (cached) return cached;
+      return fetch(request).then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE).then((cache) => cache.put(request, copy));
+        return response;
+      }).catch(() => caches.match('./index.html'));
+    })
+  );
+});
