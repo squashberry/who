ef530c9d9b2a92b3034330db32188a1b6bc7c051
Squashberrypro <squashberrypro@gmail.com>
2026-09-26 07:35:49 +0000

    Refresh download analytics dashboard cache

--- admin/sw.js
+++ admin/sw.js
@@ -1,4 +1,4 @@
-const CACHE='who-admin-v3';
+const CACHE='who-admin-v4';
 const CORE=['./','./index.html','./admin.css','./admin.js','./site.webmanifest','../favicon.svg','../icons/icon-192.png','../icons/icon-512.png'];
 self.addEventListener('install',(e)=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)));self.skipWaiting()});
 self.addEventListener('activate',(e)=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
